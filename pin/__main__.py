7bfc1feaf9c29deee1d1de4b1cbcee06f110fb5e
Mike McGann <mike.mcgann@townhallpinball.org>
2015-11-23 18:55:46 -0500

    Put procx in the correct place

--- pin/__main__.py
+++ pin/__main__.py
@@ -90,12 +90,8 @@ def init():
     if p.options["simulate"]:
         p.proc.api = virtual_proc
     else:
-        import procx
+        from pin.lib import procx
         p.proc.api = procx.PinPROC(p.platform.name)
-        #p.proc.api.driver_state_disable = pinproc.driver_state_disable.__get__(p.proc.api, p.proc.api.__class__)
-        #p.proc.api.drvier_state_pulse = pinproc.driver_state_pulse
-        #p.proc.api.driver_state_patter = pinproc.driver_state_patter
-        #p.proc.api.driver_state_pulsed_patter = pinproc.driver_state_pulsed_patter
     p.proc.init()
 
 def bind():
